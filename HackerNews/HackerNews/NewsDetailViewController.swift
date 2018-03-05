1abc09b6b4b375de99d1e2e91a27e2208cf41202
Vadim Rupets <vadik_rupets@icloud.com>
2018-03-05 18:01:06 +0300

    Created SplitViewController, NewsViewController, NewsDetailViewContoller. Deleted default ViewController

---
+++ HackerNews/HackerNews/NewsDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsDetailViewController.swift
+//  HackerNews
+//
+//  Created by Vadim Rupets on 3/5/18.
+//  Copyright © 2018 Vadim Rupets. All rights reserved.
+//
+
+import UIKit
+
+class NewsDetailViewController: UIViewController {
+
+    var story: Story?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ HackerNews/HackerNews/NewsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewsViewController.swift
+//  HackerNews
+//
+//  Created by Vadim Rupets on 3/5/18.
+//  Copyright © 2018 Vadim Rupets. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
