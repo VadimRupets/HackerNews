922e45f2a79fb228e3efe1f1bfe1c28e53f2761e
Vadim Rupets <vadzim.rupets@itechart-group.com>
2018-03-05 13:03:16 +0300

    Created Xcode project

---
+++ HackerNews/HackerNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerNews
+//
+//  Created by Vadim Rupets on 3/5/18.
+//  Copyright © 2018 Vadim Rupets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
